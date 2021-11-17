a3891739fe5cfd031ff2715306c01140cd3dc454
cjsk1126@hanmail.net <aaa@gmail.com>
2021-11-17 17:45:50 +0900

    2021.11.17 : 관리자(회원관리리스트) - el/jstl

---
+++ src/main/java/admin/AdminController.java
@@ -0,0 +1,42 @@
+package admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.ad")
+public class AdminController extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		AdminInterface command = null;
+		String viewPage = "/WEB-INF/admin";
+		
+		String uri = request.getRequestURI();
+		String com = uri.substring(uri.lastIndexOf("/"), uri.lastIndexOf("."));
+		
+		if(com.equals("/adMenu")) {
+			viewPage += "/adMenu.jsp";
+		}
+		else if(com.equals("/adLeft")) {
+			viewPage += "/adLeft.jsp";
+		}
+		else if(com.equals("/adContent")) {
+			command = new AdContentCommand();
+			command.execute(request, response);
+			viewPage += "/adContent.jsp";
+		}
+		else if(com.equals("/adMemberList")) {
+			command = new AdMemberListCommand();
+			command.execute(request, response);
+			viewPage += "/member/adMemberList.jsp";
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+}
